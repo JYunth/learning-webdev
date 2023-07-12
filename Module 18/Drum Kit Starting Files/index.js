753704122c8d205eacf9d1d8b763fba0cc924848
JYunth <jyunth28@gmail.com>
2023-07-12 18:21:52 +0530

    Drum Kit Project

---
+++ Module 18/Drum Kit Starting Files/index.js
@@ -0,0 +1,36 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        keyclicked = this.innerHTML;
+        soundPodu(keyclicked);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    soundPodu(event.key);
+});
+
+function soundPodu(keyclicked){
+    if (keyclicked == "w"){
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+    } else if (keyclicked == "a") {
+        var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+    } else if (keyclicked == "s"){
+        var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+    } else if (keyclicked == "d") {
+        var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+    } else if (keyclicked == "j"){
+        var audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+    } else if (keyclicked == "k") {
+        var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+    } else if (keyclicked == "l"){
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+    }
+}
+
