7df947096c9aab552ca171740e02590dae9a4d61
JYunth <jyunth28@gmail.com>
2023-07-13 20:33:58 +0530

    Sillyvillians and superheroes generator

---
+++ Module 23/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname";
+import superheroes from "superheroes"
+
+var sillyVillian = generateName();
+
+console.log(sillyVillian);
+
+var superhero = superheroes.random();
+
+console.log(superhero);
+
